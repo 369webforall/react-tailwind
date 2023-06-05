2622c6c8688fffe81c893179d63aeb31108fa0c5
369webforall <369webforall@gmail.com>
2023-06-05 15:10:40 +0545

    update

--- src/pages/Signin.jsx
+++ src/pages/Signin.jsx
@@ -1,7 +1,22 @@
 import React from 'react';
 
 const Signin = () => {
-  return <div>Signin</div>;
+  return (
+    <div className="flex justify-center items-center h-screen">
+      <div className="w-30">
+        <a
+          href=""
+          className="bg-blue-500 text-white text-lg font-semibold py-4 px-8 rounded hover:bg-blue-700 trasition-all duration-300 focus:ring ring-offset-2 ring-offset-blue-600"
+        >
+          Contact us
+        </a>
+
+        {/* <button className="bg-blue-500 text-white text-lg font-semibold py-4 px-8 rounded hover:bg-blue-700 trasition-all duration-300  focus:ring-2 focus:ring-offset-2 focus:ring-blue-600">
+          add product
+        </button> */}
+      </div>
+    </div>
+  );
 };
 
 export default Signin;
--- src/pages/Signup.jsx
+++ src/pages/Signup.jsx
@@ -1,7 +1,95 @@
 import React from 'react';
 
 const Signup = () => {
-  return <div>Signup</div>;
+  return (
+    <section className="flex justify-center items-center h-screen bg-slate-200">
+      <div className="bg-slate-50 p-6 rounded-xl shadow-md shadow-slate-300 w-96">
+        <form action="">
+          <h2 className="text-blue-600 text-3xl font-semibold mb-4">
+            Please Register
+          </h2>
+          <div className="flex row">
+            <div className="w-1/2 mr-1">
+              <label htmlFor="fname">First Name</label>
+              <input
+                type="text"
+                id="fname"
+                name="fname"
+                className="h-8 w-full rounded-md border-slate-300 text-sm p-2 bg-transparent outline-blue-600 shadow-md mt-1"
+              />
+            </div>
+            <div className="w-1/2 mr-1">
+              <label htmlFor="lname">Last Name</label>
+              <input
+                type="text"
+                id="lname"
+                name="lname"
+                className="h-8 w-full rounded-md border-slate-300 text-sm p-2 bg-transparent outline-blue-600 shadow-md mt-1"
+              />
+            </div>
+          </div>
+
+          {/* email */}
+
+          <div className="w-full">
+            <label htmlFor="email">Email</label>
+            <input
+              type="email"
+              id="email"
+              name="email"
+              className="h-8 w-full rounded-md border-slate-300 text-sm p-2 bg-transparent outline-blue-600 shadow-md mt-1"
+            />
+          </div>
+          <div className="w-full">
+            <label htmlFor="password">Password</label>
+            <input
+              type="password"
+              id="password"
+              name="password"
+              className="h-8 w-full rounded-md border-slate-300 text-sm p-2 bg-transparent outline-blue-600 shadow-md mt-1"
+            />
+          </div>
+          <div className="w-full">
+            <label htmlFor="confirmPassword">Confirm Password</label>
+            <input
+              type="password"
+              id="confirmPassword"
+              name="confirmPassword"
+              className="h-8 w-full rounded-md border-slate-300 text-sm p-2 bg-transparent outline-blue-600 shadow-md mt-1"
+            />
+          </div>
+          <div className="my-4">
+            <p className="text-lg">Gender</p>
+            <input
+              type="radio"
+              checked
+              name="gender"
+              id="male"
+              className="mx-1"
+            />
+            <label htmlFor="male">Male</label>
+
+            <input type="radio" name="gender" id="female" className="mx-1" />
+            <label htmlFor="female">Female</label>
+
+            <input type="radio" name="gender" id="other" className="mx-1" />
+            <label htmlFor="other">Other</label>
+          </div>
+
+          <input
+            type="Submit"
+            className="bg-blue-600 text-white block w-full py-4 px-8 cursor-pointer rounded text-2xl hover:bg-blue-800 transition-all hover:outline outline-2 outline-blue-600 outline-offset-2"
+          />
+          <p className="mt-4">
+            Aleady have a account{' '}
+            <a href="/" className="text-blue-800 text-lg font-semibold">
+              Login
+            </a>
+          </p>
+        </form>
+      </div>
+    </section>
+  );
 };
 
 export default Signup;
